03c446c1a2c7cc9a4a705ffe044104d1a68c277c
Tomas Romero Guinazu <412550@frc.utn.edu.ar>
2026-06-11 20:26:02 -0300

    update: optimized way to locate files

--- exercises/log_analyzer.py
+++ exercises/log_analyzer.py
@@ -4,9 +4,7 @@ import os
 from functools import wraps
 from pprint import pprint
 
-py_file_path = os.path.dirname(
-    os.path.abspath(__file__)  # Route of the directory where .py file is
-)
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def create_serverlog():
@@ -15,8 +13,8 @@ def create_serverlog():
     users = ["admin", "root", "support01", "operator", "supervisor"]
     crud = ["GET", "POST", "PUT", "DELETE"]
     endpoints = ["users", "products", "orders", "settings", "profile"]
-    directory = os.path.join(py_file_path, "server.log")
-    with open(directory, "w", encoding="utf-8") as log:
+
+    with open("server.log", "w", encoding="utf-8") as log:
         for i in range(30):
             messages = [
                 f"User {random.choice(users)} logged in",
@@ -25,15 +23,13 @@ def create_serverlog():
                 f"Request {random.choice(crud)} /api/v2/{random.choice(endpoints)} - {random.randint(1, 999)}ms",
             ]
             log.write(
-                f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} | {random.choice(levels)} | "
+                f"{datetime.now().isoformat(timespec='seconds')} | {random.choice(levels)} | "
                 f"{random.choice(services)} | {random.choice(messages)}\n"
             )
 
 
 def register_operation(operation_name: str):
-
     def decorator_register_operation(original_function):
-
         @wraps(original_function)
         def wrapper_register_operation(*args, **kwargs):
             start_time = datetime.now()
@@ -46,8 +42,7 @@ def register_operation(operation_name: str):
                 f"[DURATION] '{operation_name}' - {total_time.total_seconds():.4f}s\n"
             )
             print(message, end="")
-            directory = os.path.join(py_file_path, "auditory.log")
-            with open(directory, "a", encoding="utf-8") as log:
+            with open("auditory.log", "a", encoding="utf-8") as log:
                 log.write(message)
             return result
 
@@ -58,7 +53,6 @@ def register_operation(operation_name: str):
 
 @register_operation("Load logs")
 def load_logs(filepath):
-    filepath = os.path.join(py_file_path, filepath)
     try:
         with open(filepath, "r", encoding="utf-8") as f:
             keys = [
@@ -94,29 +88,26 @@ def load_logs(filepath):
 
 @register_operation("Save report")
 def save_report(logs: dict):
-    filepath = os.path.join(py_file_path, "report.csv")
     valid_registers = [
         register for register in logs if register["level"] in ("ERROR", "CRITICAL")
     ]
-    with open(filepath, "w", encoding="utf-8") as f:
+    with open("report.csv", "w", encoding="utf-8") as f:
         f.write("timestamp,level,service,message\n")
         for register in valid_registers:
             f.write(
-                f"{register["timestamp"]},{register["level"]},{register["service"]},{register["message"]}\n"
+                f"{register['timestamp']},{register['level']},{register['service']},{register['message']}\n"
             )
 
 
 @register_operation("Analyze logs")
 def analyze_logs(logs: dict) -> dict:
-    total_registers = len(logs)  # First pair
+    total_registers = len(logs)
     all_levels = [log["level"] for log in logs]
     levels = set(all_levels)
-    by_level = {level: all_levels.count(level) for level in levels}  # Second pair
+    by_level = {level: all_levels.count(level) for level in levels}
     all_services = [log["service"] for log in logs]
     services = set(all_services)
-    by_service = {  # Third pair
-        service: all_services.count(service) for service in services
-    }
+    by_service = {service: all_services.count(service) for service in services}
     all_critic_services = [
         log["service"] for log in logs if log["level"] in ("ERROR", "CRITICAL")
     ]
@@ -124,7 +115,7 @@ def analyze_logs(logs: dict) -> dict:
     amount_critic_services = {
         service: all_critic_services.count(service) for service in critic_services
     }
-    top3_critic_services = sorted(  # Fourth pair
+    top3_critic_services = sorted(
         amount_critic_services,
         key=amount_critic_services.get,
         reverse=True,
@@ -132,8 +123,8 @@ def analyze_logs(logs: dict) -> dict:
     all_hours = [log["timestamp"][11:13] for log in logs]
     hours = set(all_hours)
     registers_by_hour = {hour: all_hours.count(hour) for hour in hours}
-    most_active_hour = max(registers_by_hour, key=registers_by_hour.get)  # Fifth pair
-    ratio_error = round(len(all_critic_services) / total_registers, 4)  # Sixth pair
+    most_active_hour = max(registers_by_hour, key=registers_by_hour.get)
+    ratio_error = round(len(all_critic_services) / total_registers, 4)
     return {
         "total_registers": total_registers,
         "by_level": by_level,
